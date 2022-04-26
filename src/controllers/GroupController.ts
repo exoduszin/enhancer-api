632b32a807c80e17da7f4bf9a77c60fd1f77695d
ruj <214782@bk.ru>
2022-04-26 18:41:39 -0300

    chore: group not found error

--- src/controllers/GroupController.ts
+++ src/controllers/GroupController.ts
@@ -16,6 +16,10 @@ const show = async (request: Request, response: Response) => {
     ).then((body) => {
       const $ = load(body)
 
+      if ($('#mainContents h2').text().toLowerCase().startsWith('error')) {
+        throw new Error('The specified group could not be found')
+      }
+
       return {
         tag: $('.grouppage_header_abbrev').first().text().trim()
       }
@@ -23,31 +27,33 @@ const show = async (request: Request, response: Response) => {
     const groupData = await Fetcher(
       `${SteamHTTP.COMMUNITY}/${parseSteamGroupURL(group)}/memberslistxml`,
       { query: { xml: 1 } }
-    ).then((body) =>
-      XML2JS.parseStringPromise(body, {
-        explicitArray: false,
-        trim: true
+    )
+      .then((body) =>
+        XML2JS.parseStringPromise(body, {
+          explicitArray: false,
+          trim: true
+        })
+      )
+      .then(({ memberList }) => {
+        const groupID = new SteamID(memberList.groupID64)
+
+        const details = memberList.groupDetails
+
+        return {
+          steam_3id: groupID.getSteam3RenderedID(),
+          steam_id64: groupID.getSteamID64(),
+          custom_url: details.groupURL,
+          name: details.groupName,
+          tag: groupProfile.tag,
+          avatar: {
+            small: details.avatarIcon,
+            medium: details.avatarMedium,
+            full: details.avatarFull
+          },
+          member_count: +memberList.memberCount,
+          members: memberList.members.steamID64
+        }
       })
-    ).then(({ memberList }) => {
-      const groupID = new SteamID(memberList.groupID64)
-
-      const details = memberList.groupDetails
-
-      return {
-        steam_3id: groupID.getSteam3RenderedID(),
-        steam_id64: groupID.getSteamID64(),
-        custom_url: details.groupURL,
-        name: details.groupName,
-        tag: groupProfile.tag,
-        avatar: {
-          small: details.avatarIcon,
-          medium: details.avatarMedium,
-          full: details.avatarFull
-        },
-        member_count: +memberList.memberCount,
-        members: memberList.members.steamID64
-      }
-    })
 
     response.status(200).json(groupData)
   } catch (error) {
