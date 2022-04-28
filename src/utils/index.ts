c1a5bfdf40ff394311410e98764898017fc3a1ff
ruj <214782@bk.ru>
2022-04-28 18:59:03 -0300

    chore: steam link regex

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,9 +4,7 @@ export const createApiRoute = (path: string) =>
   `/api${!path.startsWith('/') ? '/' + path : path}`
 
 export const parseSteamProfileURL = (value: string) => {
-  const valueMatch = value.match(
-    /(?:https?:\/\/)?steamcommunity\.com\/((?:profiles|id)\/[a-zA-Z0-9_-]+)/
-  )
+  const valueMatch = value.match(steamLinkRegex(['profiles', 'id']))
   const valueParsed = Array.isArray(valueMatch) ? valueMatch[1] : value
 
   if (
@@ -26,9 +24,7 @@ export const parseSteamProfileURL = (value: string) => {
 }
 
 export const parseSteamGroupURL = (value: string) => {
-  const valueMatch = value.match(
-    /(?:https?:\/\/)?steamcommunity\.com\/((?:gid|groups)\/[a-zA-Z0-9_-]+)/
-  )
+  const valueMatch = value.match(steamLinkRegex(['gid', 'groups']))
   const valueParsed = Array.isArray(valueMatch) ? valueMatch[1] : value
 
   if (valueParsed.startsWith('103') || valueParsed.startsWith('[g:')) {
@@ -41,3 +37,10 @@ export const parseSteamGroupURL = (value: string) => {
       : valueParsed
   }
 }
+
+export const steamLinkRegex = (values: string[]) =>
+  new RegExp(
+    `(?:https?:\\/\\/)?steamcommunity\\.com\\/((?:${values.join(
+      '|'
+    )})\\/[a-zA-Z0-9_-]+)`
+  )
